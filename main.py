8161976ca651882610933a6e8ba3b121d0e64c1e
Elia Tolin <eliatolin@gmail.com>
2022-12-08 23:04:25 +0100

    add readme and requirements

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ import time
 flask_app = Flask(__name__)
 celery = make_celery(flask_app)
 
-
 @celery.task(name="simple_celery_flask_demo.long_sleep", bind=True)
 def long_sleep(self, tot_time, name):
     for i in range(tot_time):
@@ -55,16 +54,52 @@ def task_status(task_id):
 
 @flask_app.route("/time_sleep/", methods=["GET", "POST"])
 def hello():
+    tasks = celery.control.inspect()
+    active_tasks = tasks.active()
+    # for workers in active_tasks:
+    #     for t in active_tasks[workers]:
+    #         if t['name']:
+    #             if t['name'] == "simple_celery_flask_demo.long_sleep":
+    #                 task = long_sleep.AsyncResult(t['id'])
+    #                 while not task.ready():
+    #                     time.sleep(1)
+    #                     if task.info['current'] == 3:
+    #                         return jsonify({"current": task.info['current'], "task_id": task.id}), \
+    #                                202, {'Location': url_for('task_status', task_id=task.id)}
+
+    # for t in tasks.active()[0]:
+    #     if t['name']:
+    #         if t['name'] == "simple_celery_flask_demo.long_sleep":
+    #             task = long_sleep.AsyncResult(t['id'])
+    #             while not task.ready():
+    #                 time.sleep(1)
+    #                 if task.info['current'] == 3:
+    #                     return jsonify({"current": task.info['current'], "task_id": task.id}), \
+    #                            202, {'Location': url_for('task_status', task_id=task.id)}
+
     params = request.get_json(force=True)
-    time = params.get("time")
+    time_sleep = params.get("time")
     name = params.get("name")
-    task = long_sleep.apply_async(args=[time, name])
-    return jsonify({"task_id": task.id}), 202, {'Location': url_for('task_status',
-                                                                    task_id=task.id)}
+    arg = {'name':name,'tot_time':time_sleep, }
+    #arg = [time_sleep, name]
+    task = long_sleep.apply_async(kwargs=arg)
+
+    while not task.ready():
+        time.sleep(1)
+        if task.info['current'] == 3:
+            break
+
+    print(f'State={task.state}, info={task.info}')
+
+    return jsonify({"current": task.info['current'], "task_id": task.id}),\
+        202, {'Location': url_for('task_status', task_id=task.id)}
+
+
 @flask_app.route("/check/")
 def check():
     i = celery.control.inspect()
     return i.active()
+
 
 @flask_app.route('/')
 def hello_world():
